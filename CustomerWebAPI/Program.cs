2e6d233a68ef4d28fd96022cc015badfc0e3c5c7
dilendra sajini <dilendra.sajini@gmail.com>
2024-03-28 16:43:56 +0000

    Clean the project

--- CustomerWebAPI/Program.cs
+++ CustomerWebAPI/Program.cs
@@ -12,30 +12,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.MapGet("/customers", () =>
-//{
-
-//    var customerList = Enumerable.Range(1, 5).Select(index =>
-//    {
-//        var customer = new Customer();
-//        //customer.Id = index;
-//        customer.FirstName = "Jared";
-//        customer.LastName = "Jarvis";
-//        customer.CompanyName = "Sanchez-Fletcher";
-//        customer.City = "Hatfieldshire";
-//        customer.Country = "Eritrea";
-//        customer.Zip = "0094";
-//        customer.PhoneNumber1 = "274.188.8773x41185";
-//        customer.PhoneNumber2 = "001-215-760-4642x969";
-//        customer.Email = "gabriellehartman@benjamin.com";
-//        customer.Website = "https://www.mccarthy.info/";
-
-//        return customer;
-//    }).ToArray();
-//    return customerList;
-//})
-//.WithName("GetCustomerDetails")
-//.WithOpenApi();
 app.UseAuthorization();
 
 app.MapControllers();
