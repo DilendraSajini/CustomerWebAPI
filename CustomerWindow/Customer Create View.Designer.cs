424de1f67adb0521f97d2b55319dee49d091724f
dilendra sajini <dilendra.sajini@gmail.com>
2024-04-03 17:19:47 +0100

    Add basic Front End

---
+++ CustomerWindow/Customer Create View.Designer.cs
@@ -0,0 +1,399 @@
+﻿namespace CustomerWindow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            textBox11 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            textBox12 = new TextBox();
+            textBox13 = new TextBox();
+            textBox14 = new TextBox();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(label14, 0, 13);
+            tableLayoutPanel1.Controls.Add(label13, 0, 12);
+            tableLayoutPanel1.Controls.Add(label11, 0, 10);
+            tableLayoutPanel1.Controls.Add(label10, 0, 9);
+            tableLayoutPanel1.Controls.Add(label9, 0, 8);
+            tableLayoutPanel1.Controls.Add(label8, 0, 7);
+            tableLayoutPanel1.Controls.Add(label7, 0, 6);
+            tableLayoutPanel1.Controls.Add(label6, 0, 5);
+            tableLayoutPanel1.Controls.Add(label5, 0, 4);
+            tableLayoutPanel1.Controls.Add(label4, 0, 3);
+            tableLayoutPanel1.Controls.Add(label3, 0, 2);
+            tableLayoutPanel1.Controls.Add(label2, 0, 1);
+            tableLayoutPanel1.Controls.Add(textBox1, 1, 0);
+            tableLayoutPanel1.Controls.Add(textBox2, 1, 1);
+            tableLayoutPanel1.Controls.Add(textBox3, 1, 2);
+            tableLayoutPanel1.Controls.Add(textBox4, 1, 3);
+            tableLayoutPanel1.Controls.Add(textBox5, 1, 4);
+            tableLayoutPanel1.Controls.Add(textBox7, 1, 5);
+            tableLayoutPanel1.Controls.Add(textBox6, 1, 6);
+            tableLayoutPanel1.Controls.Add(textBox8, 1, 7);
+            tableLayoutPanel1.Controls.Add(textBox9, 1, 8);
+            tableLayoutPanel1.Controls.Add(textBox10, 1, 9);
+            tableLayoutPanel1.Controls.Add(textBox11, 1, 10);
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(label12, 0, 11);
+            tableLayoutPanel1.Controls.Add(textBox12, 1, 11);
+            tableLayoutPanel1.Controls.Add(textBox13, 1, 12);
+            tableLayoutPanel1.Controls.Add(textBox14, 1, 13);
+            tableLayoutPanel1.Location = new Point(182, 80);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 14;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 48F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 38F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 38F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 38F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 35F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 39F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 37F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 44F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 37F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 39F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 42F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 36F));
+            tableLayoutPanel1.Size = new Size(518, 565);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(262, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(253, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(262, 50);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(253, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(262, 97);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(253, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(262, 145);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(253, 23);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(262, 183);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(253, 23);
+            textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(262, 259);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(253, 23);
+            textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(262, 221);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(253, 23);
+            textBox7.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(262, 294);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(253, 23);
+            textBox8.TabIndex = 7;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(262, 333);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(253, 23);
+            textBox9.TabIndex = 8;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(262, 370);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(253, 23);
+            textBox10.TabIndex = 9;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(262, 414);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(253, 23);
+            textBox11.TabIndex = 10;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 15);
+            label1.TabIndex = 11;
+            label1.Text = "First Name";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 47);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 15);
+            label2.TabIndex = 12;
+            label2.Text = "Surname";
+            label2.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 94);
+            label3.Name = "label3";
+            label3.Size = new Size(61, 15);
+            label3.TabIndex = 13;
+            label3.Text = "Full Name";
+            label3.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(3, 142);
+            label4.Name = "label4";
+            label4.Size = new Size(45, 15);
+            label4.TabIndex = 14;
+            label4.Text = "Gender";
+            label4.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(3, 180);
+            label5.Name = "label5";
+            label5.Size = new Size(59, 15);
+            label5.TabIndex = 15;
+            label5.Text = "Birth Date";
+            label5.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(3, 218);
+            label6.Name = "label6";
+            label6.Size = new Size(36, 15);
+            label6.TabIndex = 16;
+            label6.Text = "Email";
+            label6.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(3, 256);
+            label7.Name = "label7";
+            label7.Size = new Size(115, 15);
+            label7.TabIndex = 17;
+            label7.Text = "Transaction Number";
+            label7.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(3, 291);
+            label8.Name = "label8";
+            label8.Size = new Size(88, 15);
+            label8.TabIndex = 18;
+            label8.Text = "Phone Number";
+            label8.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(3, 330);
+            label9.Name = "label9";
+            label9.Size = new Size(28, 15);
+            label9.TabIndex = 19;
+            label9.Text = "City";
+            label9.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(3, 367);
+            label10.Name = "label10";
+            label10.Size = new Size(36, 15);
+            label10.TabIndex = 20;
+            label10.Text = "Town";
+            label10.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(3, 411);
+            label11.Name = "label11";
+            label11.Size = new Size(44, 15);
+            label11.TabIndex = 21;
+            label11.Text = "District";
+            label11.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(3, 448);
+            label12.Name = "label12";
+            label12.Size = new Size(37, 15);
+            label12.TabIndex = 22;
+            label12.Text = "Street";
+            label12.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(3, 487);
+            label13.Name = "label13";
+            label13.Size = new Size(70, 15);
+            label13.TabIndex = 23;
+            label13.Text = "Postal Code";
+            label13.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(3, 529);
+            label14.Name = "label14";
+            label14.Size = new Size(49, 15);
+            label14.TabIndex = 24;
+            label14.Text = "Address";
+            label14.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(262, 451);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(253, 23);
+            textBox12.TabIndex = 25;
+            // 
+            // textBox13
+            // 
+            textBox13.Location = new Point(262, 490);
+            textBox13.Name = "textBox13";
+            textBox13.Size = new Size(253, 23);
+            textBox13.TabIndex = 26;
+            // 
+            // textBox14
+            // 
+            textBox14.Location = new Point(262, 532);
+            textBox14.Name = "textBox14";
+            textBox14.Size = new Size(253, 23);
+            textBox14.TabIndex = 27;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(798, 657);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "Form1";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private TextBox textBox2;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox1;
+        private TextBox textBox5;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private TextBox textBox11;
+        private Label label1;
+        private Label label2;
+        private Label label14;
+        private Label label13;
+        private Label label11;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label12;
+        private TextBox textBox12;
+        private TextBox textBox13;
+        private TextBox textBox14;
+    }
+}
---
+++ CustomerWindow/Signin.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CustomerWindow
+{
+    partial class Signin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(301, 83);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(260, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(173, 91);
+            label2.Name = "label2";
+            label2.Size = new Size(36, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Email";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(301, 139);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(260, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(173, 147);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Password";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(391, 219);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 5;
+            button1.Text = "Sign in";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(391, 337);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 6;
+            button2.Text = "Sign up";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(417, 281);
+            label4.Name = "label4";
+            label4.Size = new Size(23, 15);
+            label4.TabIndex = 7;
+            label4.Text = "OR";
+            // 
+            // Signin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Name = "Signin";
+            Text = "Signin";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private Button button1;
+        private Button button2;
+        private Label label4;
+    }
+}
